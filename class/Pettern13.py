3ed9fd70c7b13ed4ad4820c03f06d33d19ab2be3
tisha <tisha@gmail.com>
2025-08-26 10:55:13 +0530

    26-8

--- class/Pettern13.py
+++ class/Pettern13.py
@@ -1,7 +1,11 @@
-num=5
-for i in range(num):
-    print(" ")
-    for j in range(i+1):
-     print(num,end="")
-    num-= 1
-print()
+# num=1
+# for i in range(5):
+#     for j in range(i+1):
+#      print(num,end="")
+#     num+= 1
+# print()
+
+for i in range(5,0,-1):
+    for j in range(i,6):
+        print(j,end=" ")
+    print()

df90207e0360840e108e03cc2ddd04b479e0edb5
tisha <tisha@gmail.com>
2025-09-02 12:28:11 +0530

    2-9

--- Practice/Pattern20.py
+++ Practice/Pattern20.py
@@ -3,6 +3,13 @@ for i in range(1,lines+1):
     for j in range(1,lines+1):
         if i%2==0:
             print("*",end="")
+        # elif j%2!=0:
+        #     print("*",end="")
         else:
             print(" ",end="")
-    print()
+    # for i in range
+    print() 
+    # for i in range(1,lines+1):
+    #      print("* "*i)
+    # for i in range(lines-1,0,-1):
+    #     print("* "*i)

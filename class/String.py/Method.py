c71e2942eaf8702202b44283a51729f1043cf1f2
tisha <tisha@gmail.com>
2025-09-02 09:53:01 +0530

    2-9

---
+++ class/String.py/Method.py
@@ -0,0 +1,14 @@
+# st= "my name is tisha"
+# print(len(st))
+
+# st= "MY NAME IS TISHA"
+# print(str.lower(st))
+
+# st= "my name is tisha"
+# print(str.upper(st))
+
+# st= "my name is tisha"
+# print(str.capitalize(st))
+
+st= "my name is tisha"
+print(str.title(st))

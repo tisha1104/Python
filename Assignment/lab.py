0d5c9c0b8a9d1a07b48cee43af1fbd02e18af148
tisha <tisha@gmail.com>
2025-09-03 11:51:46 +0530

    3-9

---
+++ Assignment/lab.py
@@ -0,0 +1,43 @@
+#===================================================Lab-1============================================
+# Write a Python program that prints "Hello, World!".
+
+print("Hello, World!")
+
+# Set up Python on your local machine and write a program to display your name.
+
+name= "Patel Tisha"
+
+print(f"My name is {name}")
+
+#===================================================Lab-2============================================
+#Write a Python program that demonstrates the correct use of indentation, comments, and variables following PEP 8 guidelines.
+
+a=int(input("Enter The Number:-"))
+b=int(input("Enter The Number:-"))
+print(f"a={a}")
+print(f"b={b}")
+
+Addition=(a+b)
+print(f"Sum of a+b={Addition}")
+
+Subtraction=(a-b)
+print(f"Subtraction of a-b={Subtraction}")
+
+Multiplication=(a*b)
+print(f"Multiplication of a*b={Multiplication}")
+
+Division=(a/b)
+print(f"Division of a/b={Division}")
+
+Modul=(a%b)
+print(f"Moduls of a%b={Modul}")
+
+Exponentiation=(a**b)
+print(f"Exponentiation of a**b={Exponentiation}")
+
+Floor_division=(a//b)
+print(f"floor_division of a//b={Floor_division}")
+
+
+#===================================================Lab-3============================================
+

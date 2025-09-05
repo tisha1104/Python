42ada1a0717da1087da0858dc14d6848f356c3d2
tisha <tisha@gmail.com>
2025-09-05 13:21:08 +0530

    5-9

--- class/String.py/Method.py
+++ class/String.py/Method.py
@@ -31,31 +31,31 @@
 # st= "my name is tisha"
 # print(st.split(" ",1))
 
-print("ABC".join("xyz"))
+# print("ABC".join("xyz"))
 
-print("ABC".isalpha())
+# print("ABC".isalpha())
 
-print("11111".isdigit())
+# print("11111".isdigit())
 
-print("Tisha11".isalnum())
+# print("Tisha11".isalnum())
 
-print("Tisha".zfill(50))
+# print("Tisha".zfill(50))
 
-print("Tisha".center(50,"*"))
+# print("Tisha".center(50,"*"))
 
 #Find total charcters and numbers:-
 
 k="hello python hello 11111 ddss 22225"
 
-a=0
-b=0
-for i in k:
-    if str(i).isalpha():
-        a+=1
-    elif str(i).isdigit():
-        b+=1
-print("alphabet:",a)
-print("number:",b)
+# a=0
+# b=0
+# for i in k:
+#     if str(i).isalpha():
+#         a+=1
+#     elif str(i).isdigit():
+#         b+=1
+# print("alphabet:",a)
+# print("number:",b)
 
 
 #Reverse string
@@ -65,3 +65,10 @@ for i in range(len(k)-1,-1,-1):
     s+=k[i]
 
 print(s)
+
+# print(k[5:])
+# print(k[:5])
+# print(k[5:9])
+# print(k[-8:-1])
+# print(k[::-1])
+
